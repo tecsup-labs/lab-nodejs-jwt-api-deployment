29e68b2eabccc8ed6a46860d53f18674a8fb08d0
ELVERANTONY <elver.cholan@tecsup.edu.pe>
2026-05-19 08:43:34 -0500

    Listo para ir a GitHub

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Archivo index.js inicial
+require('dotenv').config()
+const http= require('http')
+
+function requestController(){
+    console.log('Bienvenidos al curso')
+}
+
+const server=http.createServer(requestController)
+
+const PORT=process.env.PORT
+
+server.listen(PORT, function(){
+    console.log("Aplicacion corriendo en: " + PORT)
+})
